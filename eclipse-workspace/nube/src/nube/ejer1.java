aece3d4d28f54281369cb87ba08b9f2074f28b72
sergio <sergio@maria-VirtualBox>
2023-05-22 11:52:45 +0200

    segundo commit

--- eclipse-workspace/nube/src/nube/ejer1.java
+++ eclipse-workspace/nube/src/nube/ejer1.java
@@ -1,5 +1,16 @@
 package nube;
 
 public class ejer1 {
+	public static void main(String[] args)
 
+	{
+
+		for (int j = 3; j > 0; j--) {
+			for (int i = 0; i < j; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
 }
